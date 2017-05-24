ec387fb8ede47cc3e835e6fa3cbbd360cc7bc3e7
Gavin Anderegg <gavin@anderegg.ca>
2017-05-24 15:46:17 -0300

    Removing React-Router and doing it myself

--- advent/src/App.js
+++ advent/src/App.js
@@ -1,53 +1,49 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import WPAPI from 'wpapi';
 import './App.css';
-
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-)
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
+			content: '',
 		};
 
 		// this._searchTextChange = this._searchTextChange.bind(this);
 		// this._handleSearch = this._handleSearch.bind(this);
 	}
 
+
 	componentDidMount() {
-		var wp = new WPAPI({
-			endpoint: 'http://localhost:8000/wp-json',
-			username: 'admin',
-			password: 'Password!'
-		});
+		if (window.location.pathname === '/') {
+			var wp = new WPAPI({
+				endpoint: 'http://localhost:8000/wp-json',
+				username: 'admin',
+				password: 'Password!'
+			});
 
-		wp.posts().slug('index').then((response) => {
-			console.log(response);
-		})
+			wp.posts().slug('index').then((response) => {
+				this.setState({
+					content: response[0].content.rendered
+				})
+			});
+		} else {
+			// window.location.pathname.slice(1).split('/').pop()
+		}
 	}
-
 
 
 	render() {
 
 
 		return (
-			<Router>
-				<div className="App">
-					<div className="App-header">
-						<h2>Welcome to React</h2>
-					</div>
-					<div className="App-content">
-
-					</div>
+			<div className="App">
+				<div className="App-header">
+					<h2>Welcome to React</h2>
 				</div>
-			</Router>
+				<div className="App-content" dangerouslySetInnerHTML={{__html: this.state.content}}></div>
+			</div>
 		);
 	}
 }
